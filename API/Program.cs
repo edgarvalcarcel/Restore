b4c284b9129aca60d5fb5b01be345c8c9f04add8
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-10 15:59:22 -0500

    git ignore file

--- API/Program.cs
+++ API/Program.cs
@@ -3,8 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// remove
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
