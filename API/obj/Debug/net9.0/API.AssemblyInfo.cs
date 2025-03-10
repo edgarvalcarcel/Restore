8b9d1447984963b75cb727576c860f0c13b1aa8f
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-10 16:13:45 -0500

    discard files

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3df78abeb0c9d1bfdbe78dd5610eeecf959a863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc577bec922aea7bb28886576e47251837a7607")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
