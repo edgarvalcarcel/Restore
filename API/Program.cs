1cedbb6c9728cc737f05c138b2a4fc3cff23ad41
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-10 16:44:53 -0500

    removed Open api support

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// //removed to suppor Openapi  Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
 app.UseHttpsRedirection();
 
