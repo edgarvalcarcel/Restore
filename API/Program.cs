9c7fd683f3a61e2e722d524e307b6fc96f44cc76
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-12 13:02:57 -0500

    new entities added, entity framework, context

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using API.Data;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // remove
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
