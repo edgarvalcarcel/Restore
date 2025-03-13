3ecbb1f3e6f2ff0970a61dd54a466955c0009fd0
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-12 19:46:30 -0500

    migration applied

---
+++ API/Data/Migrations/20250313004535_fixProducts.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityinStock",
+                table: "Products",
+                newName: "QuantityInStock");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityinStock");
+        }
+    }
+}
