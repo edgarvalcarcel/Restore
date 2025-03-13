ea3fefb30a1e0840435715ca61cbbab3b2d630ab
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-12 19:43:06 -0500

    DbInitializer products

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -4,8 +4,9 @@ public class Product
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
+    public required string PictureUrl { get; set; }
     public long Price { get; set; }
     public required string Type { get; set; }
     public required string Brand { get; set; }
-    public int QuantityinStock { get; set; }
+    public int QuantityInStock { get; set; }
 }
