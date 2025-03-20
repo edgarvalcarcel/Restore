7a27f64d8df912b7108545d862ce1dee8811fb8f
EdgarValcarcel <edgar.usa.development@outlook.com>
2025-03-20 13:31:10 -0500

    index changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,33 @@
+const products = [
+  {
+    id: 1,
+    title: 'Product 1',
+    price: 100,
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    price: 200,
+  },
+  {
+    id: 3,
+    title: 'Product 3',
+    price: 300,
+  },
+]
 function App() {
 
   return (
-    <div style={{fontSize: '1.6rem', marginTop: '2rem'}}>Re-Store</div>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.title} - ${product.price}
+          </li>
+        ))}
+      </ul>
+      </div>
   )
 }
 
