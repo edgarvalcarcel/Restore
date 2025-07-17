3ccd79f547190afadf99ac126f77fc4d6470c01e
EdgarValcarcel <edgarvalcarcel@outlook.com>
2025-07-17 18:59:35 -0500

    props properties

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -4,15 +4,15 @@ type Props={
   products: Product[];
   addProduct: () => void;
 }
-export default function Catalog(props: Readonly<Props>) {
+export default function Catalog({products,addProduct } : Readonly<Props>) {
   return (
     <>
       <ul>
-        {props.products.map((item, index) => (
-          <li key={index}>{item.name} - $ {item.price}</li>
+        {products.map((item) => (
+          <li key={item.id}>{item.name} - $ {item.price}</li>
         ))}
       </ul>
-      <button onClick={props.addProduct}>Add Product</button>
+      <button onClick={addProduct}>Add Product</button>
     </>
   )
 }
